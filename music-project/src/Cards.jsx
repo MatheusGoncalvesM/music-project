5a2d94ede5f0408af45b81f5079299482d3b1d30
Matheus Machado <matheusmachado2005@hotmail.com>
2024-10-16 17:00:14 -0300

    design das cards definido

--- music-project/src/Cards.jsx
+++ music-project/src/Cards.jsx
@@ -1,26 +1,40 @@
 import './index.css'
+import fender1 from './assets/fender1.png'
+import fenderguitar1 from './assets/fenderguitar1.png'
 
-function Cards(){
-    return(
-       <div className="section-container">
-        <div className="section-cards">
-            <img className="section-cards-icon3"></img>
-            <h1>Title</h1>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+function Cards() {
+    return (
+        <div className="section-container">
+            <div className="section-title">
+                <div className="section-line"></div>
+                <h3>Produtos em Destaque</h3>
+                <div className="section-line"></div>
+            </div>
+            <div className="section-layer">
+                <div className="section-layer-cards">
+                    <img src={fender1} height={70} style={{ objectFit: 'contain' }} alt="Fender Logo" />
+                    <img src={fenderguitar1} height={70} style={{ objectFit: 'contain' }} alt="Fender Guitar" />
+                    <h2>Guitarra Fender American Professional II Stratocaster®</h2>
+                </div>
+                <div className="section-layer-cards">
+                    <img src={fender1} height={70} style={{ objectFit: 'contain' }} alt="Fender Logo" />
+                    <img src={fenderguitar1} height={70} style={{ objectFit: 'contain' }} alt="Fender Guitar" />
+                    <h2>Guitarra Fender American Professional II Stratocaster®</h2>
+                </div>
+                <div className="section-layer-cards">
+                    <img src={fender1} height={70} style={{ objectFit: 'contain' }} alt="Fender Logo" />
+                    <img src={fenderguitar1} height={70} style={{ objectFit: 'contain' }} alt="Fender Guitar" />
+                    <h2>Guitarra Fender American Professional II Stratocaster®</h2>
+                </div>
+                <div className="section-layer-cards">
+                    <img src={fender1} height={70} style={{ objectFit: 'contain' }} alt="Fender Logo" />
+                    <img src={fenderguitar1} height={70} style={{ objectFit: 'contain' }} alt="Fender Guitar" />
+                    <h2>Guitarra Fender American Professional II Stratocaster®</h2>
+                </div>
+                
+            </div>
         </div>
-        <div className="section-cards">
-            <img className="section-cards-icon2"></img>
-            <h1>Title</h1>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-        </div>
-        <div className="section-cards">
-            <img className="section-cards-icon1"></img>
-            <h1>Title</h1>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-        </div>
-       </div> 
-    )
-
+    );
 }
 
-export default Cards
+export default Cards;
