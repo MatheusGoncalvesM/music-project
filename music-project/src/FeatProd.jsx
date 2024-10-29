a9194857495af7287a7fd8028d89330958e17b08
Matheus Machado <matheusmachado2005@hotmail.com>
2024-10-28 23:05:17 -0300

    FeatProd commit

--- music-project/src/FeatProd.jsx
+++ music-project/src/FeatProd.jsx
@@ -6,7 +6,7 @@ import ratingstars from './assets/ratingstars.png'
 function FeatProd(){
     return(
         <div className="feat-prod-container">
-            <img src={headsetimage} height={600} style={{ objectFit: 'contain' }}></img>
+            <img src={headsetimage} height={600} style={{ objectFit: 'contain', position: 'relative', left: '200px' }}></img>
             <div className="feat-prod-box">
                 <div className="feat-prod-box-info">
                     <h1>TITLE</h1>
@@ -14,7 +14,12 @@ function FeatProd(){
                     <p>There are many variations of passages of lorem Ipsum available,
                     but the majority have suffered alterations in some form, by injected humour, 
                     or randomised words which don't look even slighty believable.</p>
-                    <img src={ratingstars} height={50} style={{ objectFit: 'contain' }}></img>
+                    <img src={ratingstars} height={50} style={{ objectFit: 'contain', position: 'relative', right: '0.8vw' }}></img>
+                    <h3>$41</h3>
+                    <div className="feat-prod-box-info-button-area">
+                        <button className="button-81">Add to Card</button>
+                        <button className="button-81">More Details</button>
+                    </div>
                 </div>
             </div>
         </div>
