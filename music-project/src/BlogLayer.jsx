4dc4c8b6b108b4788ee3e09c3c74ab312f33570f
Matheus Machado <matheusmachado2005@hotmail.com>
2024-10-30 19:06:50 -0300

    .

--- music-project/src/BlogLayer.jsx
+++ music-project/src/BlogLayer.jsx
@@ -4,43 +4,8 @@ import './index.css'
 function BlogLayer(){
     return(
         <div className="blog-layer-container">
-            <h1>Recent Posts</h1>
             <div className="blog-layer-container2">
-            <div className="blog-layer-cards-container">
-                <div className="blog-layer-cards-content">
-                    <h2>Image (maybe)</h2>
-                    <h3>Title</h3>
-                    <h3>some Date and shit like that</h3>
-                </div>
-            </div>
-            <div className="blog-layer-cards-container">
-                <div className="blog-layer-cards-content">
-                    <h2>Image (maybe)</h2>
-                    <h3>Title</h3>
-                    <h3>some Date and shit like that</h3>
-                </div>
-            </div>
-            <div className="blog-layer-cards-container">
-                <div className="blog-layer-cards-content">
-                    <h2>Image (maybe)</h2>
-                    <h3>Title</h3>
-                    <h3>some Date and shit like that</h3>
-                </div>
-            </div>
-            <div className="blog-layer-cards-container">
-                <div className="blog-layer-cards-content">
-                    <h2>Image (maybe)</h2>
-                    <h3>Title</h3>
-                    <h3>some Date and shit like that</h3>
-                </div>
-            </div>
-            <div className="blog-layer-cards-container">
-                <div className="blog-layer-cards-content">
-                    <h2>Image (maybe)</h2>
-                    <h3>Title</h3>
-                    <h3>some Date and shit like that</h3>
-                </div>
-            </div>
+                
             </div>
         </div>
     )
