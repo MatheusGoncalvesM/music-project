08dd14c8cd97d1c9f4ce4d86d79001f31ea8934f
Matheus Machado <matheusmachado2005@hotmail.com>
2024-11-01 16:10:42 -0300

    framer motion instalado

--- music-project/src/BlogLayer.jsx
+++ music-project/src/BlogLayer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { motion } from 'framer-motion'
-import './index.css'
+import { React, useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
+import './index.css';
 
 function BlogLayer(){
     return(
         <div className="blog-layer-container">
-            
+            <motion.div
+                className="square"
+                animate={{ x: 200 }}
+                transition={{ ease: "easeOut", duration: 1 }}
+            ></motion.div>
         </div>
     )
 }
