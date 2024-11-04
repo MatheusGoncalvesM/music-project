4e6fccb24708ffa57ba4c63d049a552f67512863
Matheus Machado <matheusmachado2005@hotmail.com>
2024-11-04 15:37:03 -0300

    primeira animação adicionada usando framer

--- music-project/src/BlogLayer.jsx
+++ music-project/src/BlogLayer.jsx
@@ -3,15 +3,30 @@ import { motion, useInView } from 'framer-motion';
 import './index.css';
 
 function BlogLayer(){
+
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true});
+
     return(
-        <div className="blog-layer-container">
-            <motion.div
-                className="square"
-                animate={{ x: 200 }}
-                transition={{ ease: "easeOut", duration: 1 }}
-            ></motion.div>
-        </div>
-    )
+            <div ref={ref} style={{width: '100%', height: 'auto', minHeight: '50vh', display: 'flex',
+                justifyContent: 'space-evenly', alignItems: 'center' }}>
+                {[1, 2, 3].map((circle,index) => (
+                    <motion.div
+                    key={index}
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={isInView ? {opacity: 1, scale: 3} : {}}
+                    transition={{ delay: index * 0.3, duration: 0.5 }}
+                    style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: '50%',
+                        backgroundColor: 'black',
+                        margin: '0 20px',
+                    }}
+                    />
+                ))}
+            </div>
+    );
 }
 
 export default BlogLayer
