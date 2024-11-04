ec157166b69d26bb743fc78c3bc0cf9fda05a675
Matheus Machado <matheusmachado2005@hotmail.com>
2024-11-04 17:49:52 -0300

    Revert "primeira animação adicionada usando framer"
    This reverts commit 4e6fccb24708ffa57ba4c63d049a552f67512863.

--- music-project/src/BlogLayer.jsx
+++ music-project/src/BlogLayer.jsx
@@ -1,31 +1,17 @@
 import { React, useRef } from 'react';
-import { delay, motion, useInView } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 import './index.css';
 
 function BlogLayer(){
-
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true});
-
     return(
-        <>
-            <div ref={ref} className="blog-layer-container">
-                {[1].map((line, index) => (
-                    <motion.div
-                        key={index}
-                        initial={{ opacity: 0, scale: 0}}
-                        animate={ isInView ? {opacity: 1, scale: 1}: {}}
-                        transtition={{ delay: index * 0.3, duation: 0.5 }}
-                        style={{
-                            width: '5px',
-                            height: '100vh',
-                            
-                        }}
-                    />
-                ))}
-            </div>
-        </>
-    );
+        <div className="blog-layer-container">
+            <motion.div
+                className="square"
+                animate={{ x: 200 }}
+                transition={{ ease: "easeOut", duration: 1 }}
+            ></motion.div>
+        </div>
+    )
 }
 
 export default BlogLayer
