a0220271500ac825245e5c5748388aa3518e9e98
Matheus Machado <matheusmachado2005@hotmail.com>
2024-11-18 19:07:00 -0300

    .

--- music-project/src/Section1.jsx
+++ music-project/src/Section1.jsx
@@ -1,28 +1,27 @@
 import React, { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
 import './index.css';
-import manwearingphones from './assets/manwearingphones.png'
+import manwearingphones from './assets/manwearingphones.png';
 
 function Section1() {
     const ref = useRef(null);
-    const isInView = useInView(ref, { once: true, amount: 0.2 });
-
+    const isInView = useInView(ref, { once: true, amount: 0.2 }); // Monitorando a visibilidade
 
     return (
-        <div className="section-container">
+        <div className="section-container" ref={ref}>
             <div className="section-container-text">
-                <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', fontFamily: 'libre franklin' }}> Lorem ipsum dolor sit amet. 
-                Id adipisci molestiae ab rerum error et consequatur enim ea recusandae unde et nostrum itaque qui earum consequatur et distinctio officiis.
-                Sit animi quia vel nesciunt Quis et voluptate nulla eos inventore sequi sit voluptatem rerum sed libero enim aut quibusdam iure.</h3>
+                <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', fontFamily: 'libre franklin' }}>
+                    Lorem ipsum dolor sit amet. Id adipisci molestiae ab rerum error et consequatur enim ea recusandae unde et nostrum itaque qui earum consequatur et distinctio officiis.
+                    Sit animi quia vel nesciunt Quis et voluptate nulla eos inventore sequi sit voluptatem rerum sed libero enim aut quibusdam iure.
+                </h3>
             </div>
             <motion.img
-                ref={ref} 
-                src={manwearingphones} 
+                src={manwearingphones}
                 alt="Homem Usando Fone de Ouvido"
                 className="animated-image"
-                initial={{ x: "160%", opacity: 0 }} 
-                animate={isInView ? { x: "75%", opacity: 1 } : {}} 
-                transition={{ ease: "easeOut", duration: .8, delay: 0.3 }} 
+                initial={{ x: "100vw", opacity: 0 }}
+                animate={isInView ? { x: "0%", opacity: 1 } : {}} 
+                transition={{ ease: "easeOut", duration: 1.0, delay: 0.3 }}
             />
         </div>
     );
