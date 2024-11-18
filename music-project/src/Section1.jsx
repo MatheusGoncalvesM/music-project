5fa0d4b21b7f90996bef7b0daff7c2305d8ded8b
Matheus Machado <matheusmachado2005@hotmail.com>
2024-11-18 16:00:49 -0300

    .

--- music-project/src/Section1.jsx
+++ music-project/src/Section1.jsx
@@ -4,20 +4,25 @@ import './index.css';
 import manwearingphones from './assets/manwearingphones.png'
 
 function Section1() {
-    // Configura a referência e visibilidade para acionar a animação
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount: 0.2 });
 
+
     return (
         <div className="section-container">
+            <div className="section-container-text">
+                <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', fontFamily: 'libre franklin' }}> Lorem ipsum dolor sit amet. 
+                Id adipisci molestiae ab rerum error et consequatur enim ea recusandae unde et nostrum itaque qui earum consequatur et distinctio officiis.
+                Sit animi quia vel nesciunt Quis et voluptate nulla eos inventore sequi sit voluptatem rerum sed libero enim aut quibusdam iure.</h3>
+            </div>
             <motion.img
                 ref={ref} 
                 src={manwearingphones} 
-                alt="Imagem animada"
+                alt="Homem Usando Fone de Ouvido"
                 className="animated-image"
-                initial={{ x: "120%", opacity: 0 }} 
+                initial={{ x: "160%", opacity: 0 }} 
                 animate={isInView ? { x: "75%", opacity: 1 } : {}} 
-                transition={{ ease: "easeOut", duration: 1.5, delay: 0.3 }} 
+                transition={{ ease: "easeOut", duration: .8, delay: 0.3 }} 
             />
         </div>
     );
