74e96de70a74705dd69799ed1f19a63b735dc331
Matheus Machado <matheusmachado2005@hotmail.com>
2024-12-03 21:55:20 -0300

    adicionado footer básico e algumas mudanças.

--- music-project/src/Footer.jsx
+++ music-project/src/Footer.jsx
@@ -3,7 +3,18 @@ import './index.css'
 
 function Footer(){
     return(
-        <div></div>
+        <footer>
+          <ul class="footer-nav">
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Termos de uso</a></li>
+            <li><a href="#">Política de privacidade</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Ajuda</a></li>
+          </ul>
+          <div class="copyright">
+            <p>© 2024  Todos os direitos reservados.</p>
+          </div>
+      </footer>
     )
 }
 
--- music-project/src/Section1.jsx
+++ music-project/src/Section1.jsx
@@ -9,18 +9,26 @@ function Section1() {
 
     return (
         <div className="section-container" ref={ref}>
-            <div className="section-container-text">
+            {/* Animação do texto vindo da esquerda */}
+            <motion.div
+                className="section-container-text"
+                initial={{ x: "-100vw", opacity: 0 }}
+                animate={isInView ? { x: "0%", opacity: 1 } : {}}
+                transition={{ ease: "easeOut", duration: 1.0, delay: 0.3 }}
+            >
                 <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', fontFamily: 'libre franklin' }}>
                     Lorem ipsum dolor sit amet. Id adipisci molestiae ab rerum error et consequatur enim ea recusandae unde et nostrum itaque qui earum consequatur et distinctio officiis.
                     Sit animi quia vel nesciunt Quis et voluptate nulla eos inventore sequi sit voluptatem rerum sed libero enim aut quibusdam iure.
                 </h3>
-            </div>
+            </motion.div>
+
+            {/* Animação da imagem vindo da direita */}
             <motion.img
                 src={manwearingphones}
                 alt="Homem Usando Fone de Ouvido"
                 className="animated-image"
                 initial={{ x: "100vw", opacity: 0 }}
-                animate={isInView ? { x: "0%", opacity: 1 } : {}} 
+                animate={isInView ? { x: "0%", opacity: 1 } : {}}
                 transition={{ ease: "easeOut", duration: 1.0, delay: 0.3 }}
             />
         </div>
