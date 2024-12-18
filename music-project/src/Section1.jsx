4a591df76df79a1bfc217a770f28c79531e0b7e6
Matheus Machado <matheusmachado2005@hotmail.com>
2024-12-18 15:57:26 -0300

    Algumas animações removidas e adicionadas.

--- music-project/src/Section1.jsx
+++ music-project/src/Section1.jsx
@@ -20,7 +20,6 @@ function AnimatedText({ text, initialX, isInView, delay }) {
     );
 }
 
-// Componente reutilizável para Imagem Animada
 function AnimatedImage({ src, alt, initialX, isInView, delay, style }) {
     return (
         <motion.img
@@ -35,13 +34,18 @@ function AnimatedImage({ src, alt, initialX, isInView, delay, style }) {
     );
 }
 
-// Componente reutilizável para Pilares
-function Pillar({ pillarClass, bottomOffset }) {
+// Componente reutilizável para Pilar Animado
+function Pillar({ pillarClass, bottomOffset, delay, isInView }) {
     return (
-        <div className="pillar-wrapper">
+        <motion.div
+            className="pillar-wrapper"
+            initial={{ y: '100vh', opacity: 0 }}
+            animate={isInView ? { y: '0%', opacity: 1 } : {}}
+            transition={{ ease: "easeOut", duration: 0.6, delay }}
+        >
             <div className="pillarimages" style={{ bottom: bottomOffset }}></div>
             <div className={pillarClass}></div>
-        </div>
+        </motion.div>
     );
 }
 
@@ -49,30 +53,24 @@ function Section1() {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount: 0.5 });
 
-    const sampleText = `
-        Lorem ipsum dolor sit amet. Id adipisci molestiae ab rerum error et consequatur enim ea recusandae unde et nostrum itaque qui earum consequatur et distinctio officiis.
-        Sit animi quia vel nesciunt Quis et voluptate nulla eos inventore sequi sit voluptatem rerum sed libero enim aut quibusdam iure.
-    `;
+    const sampleText = `Lorem ipsum dolor sit amet. Id adipisci molestiae ab rerum error et consequatur enim ea recusandae unde et nostrum itaque qui earum consequatur et distinctio officiis.`;
 
     return (
         <>
             <div className="section-container" style={{ alignItems: 'center' }} ref={ref}>
-                <AnimatedText text={sampleText} initialX="-100vw" isInView={isInView} delay={0.1} />
-                <AnimatedImage
-                    src={manwearingphones}
-                    alt="Homem Usando Fone de Ouvido"
-                    initialX="100vw"
-                    isInView={isInView}
-                    delay={0.1}
-                />
+                <div className="section-container-text">
+                    <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', fontFamily: 'libre franklin' }}>
+                        {sampleText}
+                    </h3>
+                </div>
+                <img src={manwearingphones} alt="Homem Usando Fone de Ouvido" className="animated-image" />
             </div>
+
             <div className="section-container" style={{ alignItems: 'center' }} ref={ref}>
-                <AnimatedImage
+                <img
                     src={guitarimage2}
                     alt="Pessoa Tocando Guitarra"
-                    initialX="-100vw"
-                    isInView={isInView}
-                    delay={0.1}
+                    className="animated-image"
                     style={{
                         width: "600px",
                         height: "600px",
@@ -80,8 +78,13 @@ function Section1() {
                         clipPath: "path('M297.1 2.5c68.3 6.7 161.8 .5 191.6 98.8 30.4 61.1 5.8 156.7-42.7 214.3-48.1 57.2-115.2 75.7-178.8 82.6-66.7 7.3-136.3-7.6-180.2-62.8C40.6 281.5 23 203.3 43.7 142.4 64.3 81.5 137.8 4.6 297.1 2.5z')",
                     }}
                 />
-                <AnimatedText text={sampleText} initialX="100vw" isInView={isInView} delay={0.1} />
+                <div className="section-container-text">
+                    <h3 style={{ fontWeight: 'bold', fontSize: '1.2em', fontFamily: 'libre franklin' }}>
+                        {sampleText}
+                    </h3>
+                </div>
             </div>
+
             <div
                 className="section-container"
                 style={{
@@ -93,9 +96,9 @@ function Section1() {
                 ref={ref}
             >
                 <div className="section-container-pillars">
-                    <Pillar pillarClass="pillars pillars1" bottomOffset="120px" />
-                    <Pillar pillarClass="pillars pillars2" bottomOffset="300px" />
-                    <Pillar pillarClass="pillars pillars3" bottomOffset="120px" />
+                    <Pillar pillarClass="pillars pillars1" bottomOffset="120px" delay={0.2} isInView={isInView} />
+                    <Pillar pillarClass="pillars pillars2" bottomOffset="300px" delay={0.4} isInView={isInView} />
+                    <Pillar pillarClass="pillars pillars3" bottomOffset="120px" delay={0.6} isInView={isInView} />
                 </div>
             </div>
         </>
