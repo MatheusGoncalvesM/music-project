bab497d81922266405237790d3b904921b71151e
Matheus Machado <matheusmachado2005@hotmail.com>
2025-01-06 15:20:16 -0300

    adicionado mais imagens aos produtos.

--- music-project/src/FeatProd.jsx
+++ music-project/src/FeatProd.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import "./index.css";
 import headsetimage from "./assets/headsetimage.png";
 import ratingstars from "./assets/ratingstars.png";
+import headphones1 from "./assets/headphones1.png"
+import capotraste from "./assets/capotraste.png"
 
 const sections = [
   {
@@ -17,22 +20,22 @@ const sections = [
   {
     id: 2,
     title1: "NEW ARRIVAL",
-    title2: "GAMING PRO X",
+    title2: "Gaming Pro X",
     description:
       "A high-quality gaming headset that delivers exceptional sound clarity and comfort during extended gaming sessions.",
     originalPrice: "$59",
     discountPrice: "$49",
-    image: headsetimage,
+    image: headphones1,
   },
   {
     id: 3,
     title1: "TOP RATED",
-    title2: "AUDIO MAX 360",
+    title2: "ToneMaster Capo",
     description:
-      "Experience superior audio quality with the Audio Max 360, perfect for music lovers and audiophiles.",
-    originalPrice: "$99",
-    discountPrice: "$79",
-    image: headsetimage,
+      "Perfect grip for seamless key changes without compromising tuning. Ideal for guitar, ukulele, and other string instruments.",
+    originalPrice: "$19",
+    discountPrice: "$14",
+    image: capotraste,
   },
 ];
 
@@ -46,45 +49,49 @@ function FeatProd() {
   return (
     <div className="feat-prod-container">
       <div className="slider">
-        {sections.map((section, index) => (
-          <div
-            key={section.id}
-            className={`section ${
-              index === currentSection ? "visible" : "hidden"
-            }`}
-          >
-            <div className="feat-prod-box">
-              <img
-                src={section.image}
-                alt={section.title2}
-                style={{
-                  objectFit: "contain",
-                  position: "relative",
-                }}
-              />
-              <div className="feat-prod-box-info">
-                <h1>{section.title1}</h1>
-                <h2>{section.title2}</h2>
-                <p>{section.description}</p>
-                <img
-                  src={ratingstars}
-                  height={25}
-                  alt="Rating Stars"
-                />
-                <h3>
-                  <span className="original-price">
-                    {section.originalPrice}
-                  </span>{" "}
-                  {section.discountPrice}
-                </h3>
-                <div className="feat-prod-box-info-button-area">
-                  <button className="button-81">Add to Cart</button>
-                  <button className="button-81">More Details</button>
+        <AnimatePresence mode="wait">
+          {sections.map((section, index) =>
+            index === currentSection ? (
+              <motion.div
+                key={section.id}
+                className="section"
+                initial={{ opacity: 0, x: 0 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -100 }}
+                transition={{ duration: 0.2, ease: "easeInOut" }}
+              >
+                <div className="feat-prod-box">
+                  <img
+                    src={section.image}
+                    alt={section.title2}
+                    style={{
+                      objectFit: "contain",
+                      position: "relative",
+                    }}
+                  />
+                  <div className="feat-prod-box-info">
+                    <h1>{section.title1}</h1>
+                    <h2>{section.title2}</h2>
+                    <p>{section.description}</p>
+                    <div className="rating-stars">
+                      <img src={ratingstars} height={25} alt="Rating Stars" />
+                    </div>
+                    <h3>
+                      <span className="original-price">
+                        {section.originalPrice}
+                      </span>{" "}
+                      {section.discountPrice}
+                    </h3>
+                    <div className="feat-prod-box-info-button-area">
+                      <button className="button-81">Add to Cart</button>
+                      <button className="button-81">More Details</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        ))}
+              </motion.div>
+            ) : null
+          )}
+        </AnimatePresence>
       </div>
       <button className="button-81" onClick={handleNext}>
         Next
